4c96adec8d35ed1496f92a7a7076b01f301b44fb
Perez_99 <kweeziperez712@gmail.com>
2022-08-02 10:19:40 +0300

    templates

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import flask, render_template
+from flask import Flask, render_template
 
-app=(__name__)
+app=Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/')
+def base():
+    return render_template('base.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 
 
 if __name__ == '__main__':
